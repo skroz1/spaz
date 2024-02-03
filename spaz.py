d1ba05ba0167b3504448a9d05bbf7020b5aa056f
Scott F. Crosby <skroz1@gmail.com>
2024-02-03 14:17:48 -0500

    - "Fixed" logging to the log file, but made it uglier in the process - Implemented/added Tasker context.  Mainly to incorporate dry-run, but also   because it'll be needed when I split the main script into modules and separate   files for subcommands. - Also re-implemented config/log path handling.  Like logging, needs to be redone - token re-authorization wasn't working for getzdata.  Fixed - reworked time handling so everything is in ISO format relative to UTC - added query count to logging, just to make sure we're not abusing the API - added limits to SensorPush queries.  If limit is not specified the results will   be limited to 10 observations.  TODO fix hard coded query limit - Modified getzdata to iterate over all observations, not just recent.  This   allows us to catch up on additional datapoints that were recorded since the   last query was run, allowing for a longer frequency between queries without   losing data - Modified py-zabbix ZabbixSenter to include clock time for backfilling data - Added zabbix_server to configure subcommand

--- spaz.py
+++ spaz.py
@@ -25,8 +25,13 @@ app = typer.Typer()
 
 # Global variables for config and logger
 config = {}
-config_path = Path.home() / ".spaz" / "config.json"
-log_path = Path.home() / ".spaz" / "log"
+#config_file = Path.home() / ".spaz" / "config.json"
+#log_path = Path.home() / ".spaz" / "log"
+config_file = ""
+log_path = ""
+
+# query count for debugging
+query_count = 0
 
 # set up logging
 def setup_logging():
@@ -36,13 +41,14 @@ def setup_logging():
     # Add a handler for errors only to stderr
     logger.add(sys.stderr, level="ERROR", format="{time} {level} {message}")
 
-    # Add a file handler for all logs
-    log_path.parent.mkdir(exist_ok=True)
+    # Log everything to a file called log in log_path
+    log_file = log_path / "log"
+    logger.add(log_file, level="DEBUG", format="{time} {level} {message}")
 
 def load_config():
-    if config_path.exists():
+    if Path(config_file).exists():
         try:
-            with open(config_path, "r") as file:
+            with open(config_file, "r") as file:
                 return json.load(file)
         except json.JSONDecodeError:
             logger.error("Error decoding config file. Ensure it is valid JSON.")
@@ -51,8 +57,8 @@ def load_config():
         return {}
 
 def write_config(config):
-    config_path.parent.mkdir(parents=True, exist_ok=True)
-    with open(config_path, "w") as file:
+    config_file.parent.mkdir(parents=True, exist_ok=True)
+    with open(config_file, "w") as file:
         json.dump(config, file, indent=4)
     logger.info("Configuration saved.")
 
@@ -87,7 +93,7 @@ def refresh_access_token(refresh_token: str):
 def is_token_valid(last_updated_str: str, validity_duration: int) -> bool:
     """Check if the token is still valid based on its last updated timestamp."""
     last_updated = datetime.fromisoformat(last_updated_str)
-    return datetime.now() - last_updated < timedelta(seconds=validity_duration)
+    return datetime.now(timezone.utc) - last_updated < timedelta(seconds=validity_duration)
 
 def authenticate () -> bool:
     # Determine the need for new authentication based on token validity
@@ -102,7 +108,7 @@ def authenticate () -> bool:
             access_token = get_access_token(authorization_code)
             logger.info("Successfully authenticated and obtained access token.")
             # Update and store new tokens and timestamps in config
-            current_time_iso = datetime.now().isoformat()
+            current_time_iso = datetime.now(timezone.utc).isoformat()
             config.update({
                 "authorization_token": authorization_code,
                 "access_token": access_token,
@@ -127,7 +133,10 @@ def api_get(endpoint: str, params: dict = None) -> dict:
     try:
         response = session.get(f"{BASE_URL}/{endpoint}", headers=headers, params=params)
         response.raise_for_status()  # Raises an HTTPError for bad responses
+        global query_count
+        query_count += 1
         return response.json()
+
     except requests.HTTPError as e:
         logger.error(f"HTTP Error on GET: {e.response.status_code} {e.response.reason}")
         return {}
@@ -145,6 +154,8 @@ def api_post(endpoint: str, data: dict = None ) -> dict:
         url = f"{BASE_URL}/{endpoint}"
         response = session.post(url, headers=headers, json=data)
         response.raise_for_status()  # Raises an HTTPError for bad responses
+        global query_count
+        query_count += 1
         return response.json()
     except requests.HTTPError as e:
         logger.error(f"HTTP Error on POST: {url} {e.response.status_code} {e.response.reason}")
@@ -153,7 +164,7 @@ def api_post(endpoint: str, data: dict = None ) -> dict:
         logger.error(f"An unexpected error occurred on POST: {str(e)}")
         return {}
 
-def get_sensordata(startTime: datetime = None, sensors: list = None, stopTime: datetime = None) -> dict:
+def get_sensordata(startTime: datetime = None, sensors: list = None, stopTime: datetime = None, limit: int = None) -> dict:
     """Query SensorPush for samples based on specified time range and sensor list."""
     # Format startTime and endTime to the appropriate string format for the API
     startTime_str = startTime.isoformat() if startTime else None
@@ -170,6 +181,9 @@ def get_sensordata(startTime: datetime = None, sensors: list = None, stopTime: d
     if stopTime_str:  # Only add stopTime if stopTime is specified
         payload["stopTime"] = stopTime_str
     
+    if limit:  # Only add limit if limit is specified
+        payload["limit"] = limit
+
     # Use the api_post function to query the /samples endpoint
     response = api_post("samples", payload)
     
@@ -184,8 +198,13 @@ def send_zabbix_data(hostname: str, data: dict):
     """Send data to Zabbix using the ZabbixSender class."""
     # Create a list of ZabbixMetric objects
     metrics = []
-    for key, value in data.items():
-        metrics.append(ZabbixMetric(hostname, key, value))
+    for key, dat in data.items():
+        for item in dat:
+            value = item['value']
+            # make sure the converted value is in unix timestamp relative to utc
+            clocktime = int(parser.isoparse(item['observed']).timestamp())
+            logger.debug(f"Sending Zabbix data for {hostname}: {key} {value} at {clocktime}")
+            metrics.append(ZabbixMetric(hostname, key, value, clock=clocktime))
     
     # Create a ZabbixSender object and send the data.  TODO error checking, config management
     sender = ZabbixSender(config['zabbix_server'])
@@ -194,7 +213,7 @@ def send_zabbix_data(hostname: str, data: dict):
     except Exception as e:
         logger.error(f"Error sending data to Zabbix: {str(e)}")
         raise typer.Exit()
-        
+
     logger.info(f"Data sent to Zabbix for {hostname}.")
 
 ###
@@ -202,9 +221,25 @@ def send_zabbix_data(hostname: str, data: dict):
 ###
 
 @app.command()
-def getzdata():
+def getzdata(ctx: typer.Context):
     """Display most recent sensorpush data for all configured
     sensors and display it in a format suitable for zabbix"""
+
+    # record the time the command was STARTED, so we can store it in the config if successful.
+    # we do this early so subsequent calls don't miss any data.  This subcommand could take a while to run
+    start_time = datetime.now(timezone.utc).isoformat()
+
+    # reas and parse the last_run from the config if available
+    # TODO this probably good enough for most use cases, but if we really want to be
+    # accurate we should do this per sensor.
+    last_run = None
+    if 'last_run' in config:
+        last_run = parser.isoparse(config['last_run'])
+
+    # authenticate to SensorPush
+    if not authenticate():
+        logger.error("Authentication failed. Check configuration or service availability'.")
+        raise typer.Exit()
 
     # get a list of configured sensors from the config file.  If sensors
     # is undefined or empty, exit with an error message.
@@ -212,14 +247,22 @@ def getzdata():
         logger.error("No sensors are configured. Please run 'configsensor'.")
         raise typer.Exit()
 
+    # get the metadata for all sensors, too.  This will be used for battery voltage, rssi, etc.
+    sensormeta = api_post("devices/sensors", {} )
+
     # use get_sensordata to get the most recent data for each sensor.  Limit this to 30 minutes of data
-    now = datetime.now()
+    now = datetime.now(timezone.utc)
     try:
         #data = get_sensordata(now - timedelta(minutes=30), list(config['sensors'].keys()), now)
-        data = get_sensordata(sensors = list(config['sensors'].keys()))
+        #data = get_sensordata(sensors = list(config['sensors'].keys()))
+        logger.debug(f"Getting sensor data for sensors: {list(config['sensors'].keys())} from {last_run} to now")
+        data = get_sensordata(sensors = list(config['sensors'].keys()), stopTime = last_run, limit=1440)
     except ValueError as e:
         logger.error(f"Error getting sensor data: {str(e)}")
         raise typer.Exit()
+
+    # record whether or not data was sent to zabbix
+    data_sent = False
 
     # print the data in a format suitable for zabbix.  Iterate over the list of sensors and print the hostname
     # and the most recent sample for each hostname, measurement type, and value.
@@ -228,32 +271,77 @@ def getzdata():
         if len(data['sensors'][sensorid]) == 0:
             continue
 
+        # if the timestamp of the most recent sample is not newer than the last sample, skip it
+        if 'last_sample' in config['sensors'][sensorid]:
+            if parser.isoparse(data['sensors'][sensorid][0]['observed']) <= parser.isoparse(config['sensors'][sensorid]['last_sample']):
+                continue
+
         hostname = config['sensors'][sensorid]['hostname']
-        # the first sample in the list is the most recent
-        sample = data['sensors'][sensorid][0]
-
-        # calculate the age of the most recent sample in seconds
-        age = (datetime.now(timezone.utc) - parser.isoparse(sample['observed'])).total_seconds()
-        logger.info(f"Sample age for {hostname}: {age} seconds {sample['observed']}")
-
-        # skip if the observed value for the most recent reading is more than 5 minutes old.  TODO make this configurable
-        if datetime.now(timezone.utc) - parser.isoparse(sample['observed']) > timedelta(minutes=5):
-            continue
-
-        # for each of temparature, humidity, pressure, dewpoint, and vpd, print the hostname, measurement type, and value
         zdata = {}
-        for measurement in ['temperature', 'humidity', 'pressure', 'dewpoint', 'vpd']:
-            if measurement in sample:
-                #typer.echo(f"{hostname} sensorpush[{measurement}] {sample[measurement]}")
-                logger
-                zdata[f"sensorpush[{measurement}]"] = sample[measurement]
         
-        # send it
-        send_zabbix_data(hostname, zdata)
+        # iterate over the samples for this sensor
+        for sample in data['sensors'][sensorid]:
+            # the first sample in the list is the most recent
+            #sample = data['sensors'][sensorid][0]
 
+            typer.echo(f"Sample for {hostname}: {sample['observed']}")
+            # calculate the age of the most recent sample in seconds
+            age = (datetime.now(timezone.utc) - parser.isoparse(sample['observed'])).total_seconds()
+            logger.info(f"Sample age for {hostname}: {age} seconds {sample['observed']}")
+
+            # skip if the observed value for the most recent reading is more than 5 minutes old.  TODO make this configurable
+            #if datetime.now(timezone.utc) - parser.isoparse(sample['observed']) > timedelta(minutes=5):
+            #    continue
+
+            for measurement in ['temperature', 'humidity', 'pressure', 'dewpoint', 'vpd']:
+                if measurement in sample:
+                    #typer.echo(f"{hostname} sensorpush[{measurement}] {sample[measurement]}")
+                    logger.info(f"Sending zabbix data for {hostname}: sensorpush[{measurement}] {sample[measurement]}")
+                    # zdata is a dictionary of arrays.  Each array contains a dictionary with observed and value keys
+                    # add observed and value to the array for each measurement
+                    zdata.setdefault(f"sensorpush[{measurement}]", []).append({
+                        'observed': sample['observed'],
+                        'value': sample[measurement]
+                    })
+            
+        # add metadata for this sensor to the zabbix data  This is only run once per sensor per query.
+        for measurement in ['rssi', 'battery_voltage']:
+            if measurement in sensormeta[sensorid]:
+                #typer.echo(f"{hostname} sensorpush[{measurement}] {sensormeta[sensorid][measurement]}")
+                logger.info(f"Sending zabbix data for {hostname}: sensorpush[{measurement}] {sensormeta[sensorid][measurement]}")
+                zdata.setdefault(f"sensorpush[{measurement}]", []).append({
+                    'observed': sample['observed'],
+                    'value': sensormeta[sensorid][measurement]
+                })
+
+        # send it
+        if ctx.obj["DRY_RUN"]:
+            typer.echo(f"DRY RUN: Would send zabbix data for {hostname}: {len(zdata)} samples")
+            logger.info(f"DRY RUN: Would send zabbix data for {hostname}: {len(zdata)} samples")
+        else:
+            send_zabbix_data(hostname, zdata)
+            data_sent = True
+            # store the time of the most recent sample in the config file
+            config['sensors'][sensorid]['last_sample'] = sample['observed']
+
+    # if data was sent, save the config
+    if data_sent:
+        # save the config
+        config['last_run'] = start_time
+        write_config(config)
+
+    # debug.  print the number of queries made to the sensorpush api
+    logger.debug(f"Queries made: {query_count}")
+    #typer.echo(f"Queries made: {query_count}")
+    
 @app.command()
-def configsensor():
+def configsensor(ctx: typer.Context):
     """Configure a SensorPush sensor fpr export to zabbix"""
+
+    # authenticate to SensorPush
+    if not authenticate():
+        logger.error("Authentication failed. Check configuration or service availability'.")
+        raise typer.Exit()
 
     # if config['sensors'] doesn't exist, create it
     if 'sensors' not in config:
@@ -318,7 +406,7 @@ def configsensor():
     logger.info(f"Sensor {sensorid} configured as {hostname}")
 
 @app.command()
-def status():
+def status(ctx: typer.Context):
     """Check the status of the SensorPush service."""
     #global config
     #config = load_config()
@@ -366,36 +454,81 @@ def status():
 
 
 @app.command()
-def configure():
+def configure(ctx: typer.Context):
     #config = load_config()
+    
+    # TODO lots of error checking needed
+
     email = typer.prompt("Email", default=config.get("email", ""))
     password = getpass("Password: ")
+    zabbix_server = typer.prompt("Zabbix server", default=config.get("zabbix_server", ""))
     
     # Update the config dictionary with new values
     config.update({"email": email, "password": password})
     write_config(config)
 
 @app.callback()
-def main():
+def main(
+    ctx: typer.Context,
+    cf: str = typer.Option(
+        os.path.join(Path.home(), ".spaz/config.json"),
+        "--config",
+        "-c",
+        help="Path to the configuration file."
+    ),
+    log_dir: str = typer.Option(
+        os.path.join(Path.home(), ".spaz/"),
+        "--logdir",
+        help="Directory to store log files."
+    ),
+    dry_run: bool = typer.Option(False, "--dry-run", "-n", help="Perform a dry run without making any changes."),
+    quiet: bool = typer.Option(False, "--quiet", "-q", help="Suppress all non-error output."),
+    force: bool = typer.Option(False, "--force", "-f", help="Force update even if not recommended.")
+):
+    ctx.ensure_object(dict)
+
+    # TODO you have broken logging so badly that it's not even funny.  Fix it.
+    global log_path
+    log_path = Path(log_dir)
+
     # Set up logging
     setup_logging()
 
     # Ensure the .spaz directory exists
-    if not config_path.parent.exists():
+    global config_file
+    config_file = Path(cf)
+    if not config_file.parent.exists():
         try:
-            config_path.parent.mkdir(parents=True)
+            cfpath.parent.mkdir(parents=True)
         except PermissionError:
-            logger.error(f"Could not create the directory {config_path.parent}. Please check permissions.")
+            logger.error(f"Could not create the directory {cfpath.parent}. Please check permissions.")
             raise typer.Exit()
 
     # Load the configuration
     global config
     config = load_config()
+    
+    # also store the config in the context object
+    ctx.obj["CONFIG"] = config
+
     logger.info("Spaz started.")
 
     # create a session to use for all requests
     global session
     session = requests.Session()
+
+    # record remaining options in the context object
+    ctx.obj["LOG_DIR"] = Path(log_path)
+    ctx.obj["DRY_RUN"] = dry_run
+    ctx.obj["QUIET"] = quiet
+    ctx.obj["FORCE"] = force
+
+app.command()(configure)
+app.command()(status)
+app.command()(getzdata)
+app.command()(configsensor)
+
+
 
 if __name__ == "__main__":
     app()
